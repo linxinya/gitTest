1fa743e482bece730583200d84bb9d9c98a85056
zhaolin07 <lin15232408067+->
2018-08-11 15:43:54 +0800

    第三次增加

--- java/com/meituan/test/Test.java
+++ java/com/meituan/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public int hashCode() {
         System.out.println("第一个增加");
         System.out.println("第二个增加");
+        System.out.println("第三次增加");
         return super.hashCode();
     }
 }
