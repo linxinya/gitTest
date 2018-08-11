bcd7a2cca4babcb9c62f7500468dbc399c1556ef
zhaolin07 <lin15232408067+->
2018-08-11 15:32:36 +0800

    第二次增加

--- java/com/meituan/test/Test.java
+++ java/com/meituan/test/Test.java
@@ -4,6 +4,7 @@ public class Test {
     @Override
     public int hashCode() {
         System.out.println("第一个增加");
+        System.out.println("第二个增加");
         return super.hashCode();
     }
 }
